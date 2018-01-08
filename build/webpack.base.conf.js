4b0ed1d98b1414b10f769b8e7f20c06adff5b37c
Foy Lian <ljs2342003@gmail.com>
2018-01-08 11:09:41 +0800

    foo bar

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,7 +8,9 @@ function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-const createLintingRule = () => ({
+const createLintingRule = () => ({}
+/*  close eslin
+{
   test: /\.(js|vue)$/,
   loader: 'eslint-loader',
   enforce: 'pre',
@@ -17,7 +19,8 @@ const createLintingRule = () => ({
     formatter: require('eslint-friendly-formatter'),
     emitWarning: !config.dev.showEslintErrorsInOverlay
   }
-})
+}*/
+)
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,50 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">
+              消息中心
+          </el-menu-item>
+          <el-menu-item index="3">论坛</el-menu-item>
+        </el-menu>
+      </el-header>
       <el-main>
         <router-view/>
       </el-main>
-      <el-footer>Footer</el-footer>
+      <el-footer>
+        <span>
+          求打赏：i6W9ktjofNj6KbFm6NvsSkrqKWSRMkZCUs
+          <h4>copy right: ABCD@qq.com</h4>
+        </span></el-footer>
     </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+      return {
+        activeIndex :  '1'
+      }
+  }
 }
 </script>
 
 <style>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
+  .el-header {
     color: #333;
     text-align: center;
     line-height: 60px;
   }
+.el-footer{
+  background-color: #393d49;
+  color: #fff;
+  text-align: center;
+  font-weight: 500;
+  margin-top: -8px;
+}
 
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
-
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
 </style>
